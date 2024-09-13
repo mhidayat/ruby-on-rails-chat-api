ae339fc15afbfd26d53a613f60ee671c8a577763
dayat <dayatsmkn26@gmail.com>
2024-09-13 19:33:41 +0700

    push local code

---
+++ app/models/message.rb
@@ -0,0 +1,13 @@
+class Message < ApplicationRecord
+  after_create_commit { broadcast_message }
+
+  private
+
+  def broadcast_message
+    ActionCable.server.broadcast("MessagesChannel", {
+      id:,
+      username:,
+      body:
+    })
+  end
+end
